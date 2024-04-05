e0de51fc88a2d4604d18cf44b826584e3b37eab4
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2024-04-05 16:43:04 +0300

    config production database

---
+++ config/env/production/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+    connection: {
+        client: 'mysql',
+        connection: {
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 3306),
+            database: env('DATABASE_NAME', 'magnetdev'),
+            user: env('DATABASE_USERNAME', 'magnet_next'),
+            password: env('DATABASE_PASSWORD', ' M@gn3t78#@!'),
+            ssl: env.bool('DATABASE_SSL', false),
+        },
+    },
+});
